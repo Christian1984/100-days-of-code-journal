efe403baa855c42a1cebc826ff0dcfc1d8a87e2d
Chris <chris.hoffmann84@gmail.com>
2017-07-03 23:10:52 +0200

    - react installed and added to package.json, - fourseven:scss added, - project cleanup, - very basic jsx mockup added

--- 100-days-of-code-journal/client/main.js
+++ 100-days-of-code-journal/client/main.js
@@ -1,22 +1,36 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    console.log('client started!');
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+    let jsx=(
+      <div>
+        <div className='title-bar'>
+          <h1>100 Days Of Code Journal</h1>
+          <h2>Made by Chris</h2>
+        </div>
+        <div className='control-bar'>
+          <button>Play/Pause</button>
+          <div>TIME</div>
+          <button>Finish</button>
+        </div>
+        <div className='progress-gauge'></div>
+        <div className='journal-list'>
+          <div className='journal-item'>
+            <p>Date: 07.01.1984</p>
+            <p>Duration: 1:00:07</p>
+            <p>Log: Lorem ipsum</p>
+            <p>Links: http://example.com</p>
+            <button>Edit</button>
+          </div>
+        </div>
+      </div>
+    );
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
