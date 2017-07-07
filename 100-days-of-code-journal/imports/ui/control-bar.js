8f8cc974ab22c91037977605d0da30275f790e24
Chris <chris.hoffmann84@gmail.com>
2017-07-07 16:02:27 +0200

    - timer added

--- 100-days-of-code-journal/imports/ui/control-bar.js
+++ 100-days-of-code-journal/imports/ui/control-bar.js
@@ -4,18 +4,61 @@ import PropTypes from 'prop-types';
 import Moment from 'moment';
 
 export default class ControlBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSeconds: 0 ,
+      isRunning: false
+    }
+  }
+
+  stopWatchTick() {
+    console.log('tick');
+
+    if (this.state.isRunning) {
+      this.setState({currentSeconds: this.state.currentSeconds + 1});
+      setTimeout(this.stopWatchTick.bind(this), 1000);
+    }
+  }
+
+  startStopwatch() {
+    this.setState({isRunning: true}, () => {setTimeout(this.stopWatchTick.bind(this), 1000);});
+  }
+
+  stopStopwatch() {
+    this.setState({isRunning: false});
+  }
+
+  toggleStopwatch() {
+    if (this.state.isRunning) {
+      this.stopStopwatch();
+    }
+    else {
+      this.startStopwatch();
+    }
+  }
+
+  addLeadingZero(val) {
+    return val < 10 ? `0${val}` : val;
+  }
+
+  getTimerStringFromSeconds(s) {
+    let hh = this.addLeadingZero(~~(s / 3600));
+    let mm = this.addLeadingZero(~~((s % 3600) / 60));
+    let ss = this.addLeadingZero(s % 60);
+    return `${hh}:${mm}:${ss}`;
+  }
+
   getTotalTime() {
     let durationList = this.props.journalEntries.map((entry) => {
       return 60 * entry.duration.h + 1 * entry.duration.m;
     });
 
-    var totalMinutes=0;
-    for(var i in durationList) { totalMinutes+=durationList[i]; }
+    var totalSeconds = this.state.currentSeconds;
+    for(var i in durationList) { totalSeconds+=durationList[i] * 60; }
 
-    return {
-      h: ~~(totalMinutes / 60),
-      m: totalMinutes % 60
-    };
+    return this.getTimerStringFromSeconds(totalSeconds);
   }
 
   render() {
@@ -24,16 +67,16 @@ export default class ControlBar extends React.Component {
         <div className='control-bar'>
           <div className='wrapper'>
             <div className='control-bar__flex-wrapper'>
-              <button className='button'>Play/Pause</button>
+              <button className='button' onClick={this.toggleStopwatch.bind(this)}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
               <div className='control-bar__timer label-data-pair'>
                 <div className='label-data-pair__label'>Time (Today):</div>
-                <div className='label-data-pair__data'>00:00:00</div>
+                <div className='label-data-pair__data'>{this.getTimerStringFromSeconds(this.state.currentSeconds)}</div>
               </div>
               <div className='control-bar__timer label-data-pair'>
                 <div className='label-data-pair__label'>Time (Total): </div>
-                <div className='label-data-pair__data'>{`${totalTime.h}:${totalTime.m}:00`}</div>
+                <div className='label-data-pair__data'>{this.getTotalTime()}</div>
               </div>
-              <button className='button'>Finish</button>
+              <button className='button' onClick={this.stopStopwatch.bind(this)}>Finish</button>
             </div>
           </div>
         </div>
